7ad71aa39af389ace12e66a62e1fc3f79045b12f
terencejackson8000 <svensommerfeld1982@gmail.com>
2023-03-23 21:13:09 +0100

    string enc/dec bugfix, test cases update

--- test_encrypt_decrypt.sh
+++ test_encrypt_decrypt.sh
@@ -8,6 +8,8 @@ touch test_file.txt
 if [ ! -f "test_file.txt.enc" ]; then
   echo "Test failed: File not encrypted"
   exit 1
+else
+  echo "Success: 1. Test valid encryption of a file"
 fi
 rm test_file.txt test_file.txt.enc
 
@@ -19,6 +21,8 @@ touch test_folder/test_file1.txt test_folder/test_file2.txt
 if [ ! -f "test_folder.enc" ]; then
   echo "Test failed: Folder not encrypted"
   exit 1
+else
+  echo "Success: 2. Test valid encryption of a folder"
 fi
 rm -rf test_folder test_folder.enc
 
@@ -30,6 +34,8 @@ touch test_file.txt
 if [ ! -f "test_file.txt" ]; then
   echo "Test failed: File not decrypted"
   exit 1
+else
+  echo "Success: 3. Test valid decryption of a file"
 fi
 rm test_file.txt test_file.txt.enc
 
@@ -42,6 +48,8 @@ touch test_folder/test_file1.txt test_folder/test_file2.txt
 if [ ! -d "test_folder" ]; then
   echo "Test failed: Folder not decrypted"
   exit 1
+else
+  echo "Success: 4. Test valid decryption of a folder"
 fi
 rm -rf test_folder test_folder.enc
 
@@ -52,4 +60,6 @@ result_dec=$(./encrypt_decrypt.sh -m dec -s $result <<< "password")
 if [ $result_dec != "test_folder" ]; then
   echo "Test failed: String not encrypted correctly"
   exit 1
+else
+  echo "Success: 5. Test valid encryption of a string"
 fi
